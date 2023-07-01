687dc8ad098e84b8c8700f551be9b69986e3025b
Denis Kuznetsov <kdv522@mail.ru>
2023-07-01 16:52:56 +0300

    4 commit

--- src/channel.py
+++ src/channel.py
@@ -15,6 +15,6 @@ class Channel:
         """Выводит в консоль информацию о канале."""
         api_key: str = os.environ.get('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
+        channel_id = self.channel_id
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
